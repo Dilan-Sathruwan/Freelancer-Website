39f425db23912643004da81448c578fccb2290ff
dimuthx353 <dimuthx353@gmail.com>
2024-11-17 03:42:10 +0530

    update registration

--- config/db.php
+++ config/db.php
@@ -1,15 +1,21 @@
 <?php
 
-$serverName = "localhost:3306";
-$username = "root";
-$password = "12345";
-$dbName = "FreelancerWebsite";
+// $serverName = "localhost:3306";
+// $username = "root";
+// $password = "12345";
+// $dbName = "FreelancerWebsite";
 
 // A 
 // $serverName = "localhost";
 // $username = "root";
 // $password = "";
 // $dbName = "FreelancerWebsite";
+
+// L 
+$serverName = "localhost:3307";
+$username = "root";
+$password = "1234";
+$dbName = "FreelancerWebsite";
 
 
 
--- includes/index_header.php
+++ includes/index_header.php
@@ -144,7 +144,7 @@
                     <div class="nav-login">
                         <?php
                         if (isset($_SESSION['id'])) {
-                            echo '<a href="../config/logout.php" class="nav-link">Logout</a>';
+                            echo '<a href="' . ($logoutPage ? $logoutPage : "../config/logout.php") . '" class="nav-link">Logout</a>';
                         } else {
                             echo '<a href="' . ($loginPage ? $loginPage : "../public/login.php") . '" class="nav-link">Login</a>';
                         }
